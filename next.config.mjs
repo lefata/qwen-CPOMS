f47542b9f2c4f1482338e475989715c784ced5d4
lefata <franklefata@gmail.com>
2026-08-06 14:10:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 // If your files are in /app (root)
 /** @type {import('next').NextConfig} */
-const nextConfig = {}; 
-module.exports = nextConfig;
+const nextConfig = {};
+
+// ✅ Use ES Module export syntax for .mjs files
+export default nextConfig;
